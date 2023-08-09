398fb527c64e765256dd1b14c998b617e94983be
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-08-09 16:00:06 -0600

    Nuevo

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 import mysql from "mysql2/promise"
 
 import { PORT,DB_DATABASE,DB_PASSWORD,DB_HOST,DB_PORT,DB_USER } from "./config.js"
-
 export const conexion = mysql.createPool({
   host:DB_HOST,
   user:DB_USER,
